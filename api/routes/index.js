3428c33249d393afec65fe32e8e547fb968bc1a9
Allan de Miranda Silva <allandemiranda@gmail.com>
2020-07-29 16:50:03 -0300

    verificando interação

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,33 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const SerialPort = require("serialport");
+const Readline = require("@serialport/parser-readline");
+const Delimiter = require("@serialport/parser-delimiter");
+const { listenerCount } = require("serialport");
+const port = new SerialPort("/dev/ttyACM0", { baudRate: 9600 });
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", async (req, res, next) => {
+  //res.render('index', { title: 'Express' });
+
+  port.write("1");
+
+  teste = { };
+
+  const parser = port.pipe(new Readline({ delimiter: "\r\n" }));
+
+  function receberInfo(port, parser) {
+    return new Promise(function (resolve, reject) {
+        parser.on("data", (data) => {
+          resolve(JSON.parse(data));
+        });      
+    });
+  }
+
+  await receberInfo(port, parser).then(async (data, err) => {console.log(data); teste=data });
+
+  res.json(teste);
 });
 
 module.exports = router;
